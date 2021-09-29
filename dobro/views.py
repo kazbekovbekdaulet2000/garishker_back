b575e1f6e7e7629b45b661b46f9dbfb50ffd05b8
Azat <58590243+azatnt@users.noreply.github.com>
2021-09-30 01:34:32 +0600

    added urls to dobro

--- dobro/views.py
+++ dobro/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import *
+from rest_framework.response import *
+from rest_framework import status
 
-# Create your views here.
+
+class Index(APIView):
+    def get(self, request):
+        return Response("Success", status=200)
