cf46ceca71e28826f4012492092b65b033c81b3d
Bauyrzhan <51294316+gabitovichb@users.noreply.github.com>
2021-10-14 15:13:57 +0600

    Add comment and like

--- obrazovanie/urls.py
+++ obrazovanie/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('reports', ReportView.as_view(), name='report_urls'),
     path('videos', VideoView.as_view(), name='video_urls'),
     path('search_data', SearchView.as_view(), name='search_urls'),
+    path('like/', LikeView.as_view(), name='like_post'),
+    path('unlike/', UnlikeView.as_view(), name='unlike_post'),
+    path('comment/', CommentCreate.as_view(), name='comment_create'),
+    path('delete/', CommentDelete.as_view(), name='comment_delete'),
+    path('comments/', CommentList.as_view(), name='comments'),
+
 ]
