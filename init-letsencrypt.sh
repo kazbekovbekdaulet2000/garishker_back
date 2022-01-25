08b282c7a89aee970ccbf40ce709f983c38b1a20
Bekdaulet <Kazbekov.bekdaulet2000@gmail.com>
2022-01-25 16:34:15 +0600

    vulnarabilities

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,10 +4,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(app.garyshker-app.kz)
+domains=()
 rsa_key_size=4096
 data_path="./data/certbot"
-email="kazbekov.bekdaulet2000@gmail.com" # Adding a valid address is strongly recommended
+email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
