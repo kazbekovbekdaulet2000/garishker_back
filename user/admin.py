b3baae026834cd31699195e4dfee8baf9c81b8c2
Бекдаулет Казбеков <k_bekdaulet@MacBook-Air-Bekdaulet.local>
2022-03-27 18:28:42 +0600

    user change restriction in admin

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name', 'surname', 'created_at']
+    ordering = ['-created_at']
+    readonly_fields = ('email', 'name', 'surname', 'birth_date',
+                       'image', 'city', 'description', 'user_type')
+
+
+admin.site.register(User, UserAdmin)
