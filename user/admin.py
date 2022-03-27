f42554bb24856a210ea19184fbdb2fcb831a101f
Бекдаулет Казбеков <k_bekdaulet@MacBook-Air-Bekdaulet.local>
2022-03-27 18:59:00 +0600

    add filter and search for users

--- user/admin.py
+++ user/admin.py
@@ -3,10 +3,12 @@ from .models import *
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'name', 'surname', 'created_at']
+    list_display = ['email', 'name', 'surname', 'created_at', 'is_staff']
     ordering = ['-created_at']
     readonly_fields = ('email', 'name', 'surname', 'birth_date',
                        'image', 'city', 'description', 'user_type')
+    list_filter = ['is_staff', 'verified']
+    search_fields = ['email', 'name', 'surname']
 
 
 admin.site.register(User, UserAdmin)
