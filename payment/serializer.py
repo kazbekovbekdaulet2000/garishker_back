b6a07c15b91a923178f2973c56e8000e3f93c443
Bekdaulet <kazbekov.bekdaulet2000@gmail.com>
2022-05-02 06:08:01 +0600

    feat: demo payment

---
+++ payment/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from payment.models import Donation
+
+
+class DonationCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donation
+        fields = "__all__"
--- payment/views.py
+++ payment/views.py
@@ -1,14 +1,8 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.request import Request
 from payment.models import Donation
 
 
 class CreateDonation(APIView):
     def post(self, request, *args, **kwargs):
-        print(request.path)
-        print(request.META)
-
-        # return Response({'hello'})
-        return Response(Donation.createOrder(10349832, "AUTO"))
+        return Response(Donation.createOrder(request.data['amount'], "https://garyshker-dev.web.app/", "AUTO"))
